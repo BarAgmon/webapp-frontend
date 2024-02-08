4006b8f1027d2e55fa2a846e88eb630efa76544f
Bar Agmon <111876103+m23anon@users.noreply.github.com>
2024-02-08 15:48:20 +0200

    Added currency exchange calculator

--- src/pages/CurrencyExchange.tsx
+++ src/pages/CurrencyExchange.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {convertCurrency, supportedCurrencies} from '../services/currency-api'
-import { Button } from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+import MoneyPng from '../images/image.jpg';
 
 function CurrencyExchange() {
   const [fromCurrency, setFromCurrency] = useState('USD');
@@ -13,31 +16,67 @@ function CurrencyExchange() {
     }
 
   return (
-    <div>
+    <PagePosition>
       <h2>Currency Exchange</h2>
-      <div className="form-floating">
-        <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(+e.target.value)}
-            placeholder="Amount"
-            className="form-control" 
-            id="floatingInputGrid"
-        />
-        <label htmlFor="floatingInputGrid">Amount</label>
-      </div>
-      <select className="form-select" id="floatingSelect" value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
-      {supportedCurrencies.map((currency) => (<option value="currency">{currency}</option>))}
-      </select>
-      <span> to </span>
-      <select className="form-select" id="floatingSelect" value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-      </select>
-      <Button onClick={handleConvert} variant="outline-light">Convert</Button>
-      <p>{result}</p>
-    </div>
+      <DesignedCard>
+        <Card.Body>
+          <InputDiv className="form-floating">
+            <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(+e.target.value)}
+                placeholder="Amount"
+                className="form-control" 
+                id="floatingInputGrid"
+            />
+            <label htmlFor="floatingInputGrid">Amount</label>
+          </InputDiv>
+          <InputDiv className="form-floating">
+            <select className="form-select" id="fromCurrency" value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
+              {supportedCurrencies.map((currency) => (<option value={currency} key={currency}>{currency}</option>))}
+            </select>
+            <label htmlFor="fromCurrency">From</label>
+          </InputDiv>
+          <InputDiv className="form-floating">
+            <select className="form-select" id="toCurrency" value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+              {supportedCurrencies.map((currency) => (<option value={currency} key={currency}>{currency}</option>))}
+            </select>
+            <label htmlFor="toCurrency">To</label>
+          </InputDiv>
+          <InputDiv>
+            <Alert key="result" variant="success" show={result != ""} onClose={() => setResult("")} dismissible>{result}</Alert>
+          </InputDiv>
+          <InputDiv>
+            <Button onClick={handleConvert} variant="dark">Convert</Button>
+          </InputDiv>
+        </Card.Body>
+      </DesignedCard>
+    </PagePosition>
   );
 };
 
 export default CurrencyExchange;
+
+
+const DesignedCard = styled(Card)`
+  width: 40em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${MoneyPng});
+`
+const PagePosition = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100vw;
+    height: 100vh;
+`
+const InputDiv = styled.div`
+  width: 20em;
+  margin-top: 2em;
+`
